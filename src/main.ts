8c28bb55b5e75adf410c56530ae97af41dfcdb6f
Marquis <abahmarquis@gmail.com>
2026-06-02 15:29:04 +0100

    fixed backend

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
